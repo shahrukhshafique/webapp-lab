237eee77f042c7fd9800d9fd009c79a95cef84ba
U-SYSTEMS\shahrukh.shafique <shahrukh.shafique@LHRLT-12273.Systemsltd.local>
2024-11-28 19:04:16 +0500

    adding webapp into repo

---
+++ src/main/java/site/loadtesting/dapp/DappApplication.java
@@ -0,0 +1,13 @@
+package site.loadtesting.dapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/loadtesting/dapp/DappApplicationTests.java
@@ -0,0 +1,13 @@
+package site.loadtesting.dapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
